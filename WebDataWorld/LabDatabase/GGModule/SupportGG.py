e4a3ba60be4a57912b47d40163e2f8aa92c5b028
Vivian-WEI-0408 <619009418@qq.com>
2026-07-10 10:53:17 +0800

    update

--- WebDataWorld/LabDatabase/GGModule/SupportGG.py
+++ WebDataWorld/LabDatabase/GGModule/SupportGG.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import dnacauldron
+import traceback
 
 
 
@@ -11,16 +12,20 @@ class SupportGG:
         self.repository = dnacauldron.SequenceRepository()
         print(self.PartName_List)
 
-    def assemblyPart(self,name):
+    def assemblyPart(self,name,enzyme = "auto"):
         temp = len(self.repository.get_all_part_names())
         if(len(self.repository.get_all_part_names())!=0):
             self.repository = dnacauldron.SequenceRepository()
         print(f"part_file_address:{self.PartAddress_List}")
         print(f"part_name:{self.PartName_List}")
-        self.repository.import_records(files=self.PartAddress_List,use_file_names_as_ids=True, topology='default_to_circular')
-        assembly = dnacauldron.Type2sRestrictionAssembly(parts=self.PartName_List, name = name)
-        # assembly = dnacauldron.Type2sRestrictionAssembly(parts=self.PartName_List,expect_no_unused_parts=False)
-        self.simulation = assembly.simulate(sequence_repository=self.repository)
+        try:
+            self.repository.import_records(files=self.PartAddress_List,use_file_names_as_ids=True, topology='default_to_circular')
+            assembly = dnacauldron.Type2sRestrictionAssembly(parts=self.PartName_List, name = name, enzyme = enzyme)
+            # assembly = dnacauldron.Type2sRestrictionAssembly(parts=self.PartName_List,expect_no_unused_parts=False)
+            self.simulation = assembly.simulate(sequence_repository=self.repository)
+        except Exception as e:
+            traceback.print_exc()
+            print(e.args)
         # assert len(self.simulation.construct_records) == 1
         # assert len(self.simulation.construct_records[0]) == 8016
 
@@ -36,8 +41,8 @@ class SupportGG:
         self.repository.add_records(records)
 
 if __name__ == '__main__':
-    AddressList = ['C:\\Users\\admin\\Desktop\\WebDatabase\\WebDataWorld\\LabDatabase\\static\\LabDatabase\\DownloadFile\\GenerateFile\\AssemblyFile\\\\backbone-pScbb01.gbk', 'C:\\Users\\admin\\Desktop\\WebDatabase\\WebDataWorld\\LabDatabase\\static\\LabDatabase\\DownloadFile\\GenerateFile\\AssemblyFile\\\\pScP01.gbk']
-    FileName = ['backbone-pScbb01', 'pScP01']
+    AddressList = [r'WebDataWorld/output/50f10337-269d-4961-841b-fbc7127dcfc1/ZZ085/provided_parts_records/backbone-769-ZZ031-.gb',r'WebDataWorld/output/50f10337-269d-4961-841b-fbc7127dcfc1/ZZ085/provided_parts_records/plasmid-8244-ZZ014-phiBT1 Integrase.gb']
+    FileName = ['backbone-769-ZZ031-', 'plasmid-8244-ZZ014-phiBT1 Integrase']
     test = SupportGG(AddressList,FileName)
-    test.assemblyPart("webtest")
+    test.assemblyPart("webtest","BsaI")
     test.show()
